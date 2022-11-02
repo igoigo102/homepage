87de13fd1785c0e6011b1a503aad4b9765106420
JuSeong Lee <wntjd102@naver.com>
2022-11-02 20:42:00 +0900

    Initial commit

---
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class MemberRepositoryTest {
+
+}
---
+++ src/test/java/jpabook/jpashop/service/ItemServiceTest.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.service;
+
+import jdk.jshell.spi.ExecutionControlProvider;
+import jpabook.jpashop.domain.item.Book;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ItemServiceTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void updateTest() throws Exception {
+        Book book = em.find(Book.class, 1L);
+
+        //TX
+        book.setName("asdfasfasdfasdfsad");
+
+        //변경감지 == dirty checking
+        //TX commit
+
+
+    }
+
+
+
+}
